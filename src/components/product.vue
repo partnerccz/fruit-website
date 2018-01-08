3cd7726f05c109590cfc8998c6c30e683d9d0231
zzzzgc <1098766713@qq.com>
2018-01-08 19:06:24 +0800

    添加关于我们和商品服务

---
+++ src/components/product.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="product">
+    <div class="display_fruit">
+      <div v-for="(fruitsClass, index) in fruitsSetting">
+        <h2>{{fruitsClass.title}}</h2>
+        <table class="tableClass">
+          <div v-for="(fruitsTr, index) in fruitsClass.fruits">
+            <tr>
+              <td class="truit_item" v-for="(fruitsTd, index) in fruitsTr">
+                <img :src="fruitsTd.img_url" alt="" class="fruit_img">
+                <h3 class="fruit_name_str" v-text="fruitsTd.fruits_name"></h3>
+              </td>
+            </tr>
+          </div>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'product',
+    data: function () {
+      return {
+        fruitsSetting: [
+          {
+            title: '进口水果',
+            fruits: [
+              [
+                {
+                  img_url: 'http://file06.16sucai.com/2016/0502/19b572a189ec13d9cdcac82f579d680a.jpg',
+                  fruits_name: '干货'
+                },
+                {
+                  img_url: 'http://file06.16sucai.com/2016/0502/19b572a189ec13d9cdcac82f579d680a.jpg',
+                  fruits_name: '干货'
+                },
+                {
+                  img_url: 'http://file06.16sucai.com/2016/0502/19b572a189ec13d9cdcac82f579d680a.jpg',
+                  fruits_name: '干货'
+                },
+                {
+                  img_url: 'http://file06.16sucai.com/2016/0502/19b572a189ec13d9cdcac82f579d680a.jpg',
+                  fruits_name: '干货'
+                }
+              ],
+              [
+                {
+                  img_url: 'http://file06.16sucai.com/2016/0502/19b572a189ec13d9cdcac82f579d680a.jpg',
+                  fruits_name: '干货'
+                },
+                {
+                  img_url: 'http://pic13.nipic.com/20110301/2531170_082157389664_2.jpg',
+                  fruits_name: '干货'
+                },
+                {
+                  img_url: 'http://file06.16sucai.com/2016/0502/19b572a189ec13d9cdcac82f579d680a.jpg',
+                  fruits_name: '干货'
+                },
+                {
+                  img_url: 'http://file06.16sucai.com/2016/0502/19b572a189ec13d9cdcac82f579d680a.jpg',
+                  fruits_name: '干货'
+                }
+              ]
+            ]
+          },
+          {
+            title: '国内精品水果',
+            fruits: [
+              [
+                {
+                  img_url: 'http://file06.16sucai.com/2016/0502/19b572a189ec13d9cdcac82f579d680a.jpg',
+                  fruits_name: '干货'
+                },
+                {
+                  img_url: 'http://file06.16sucai.com/2016/0502/19b572a189ec13d9cdcac82f579d680a.jpg',
+                  fruits_name: '干货'
+                },
+                {
+                  img_url: 'http://file06.16sucai.com/2016/0502/19b572a189ec13d9cdcac82f579d680a.jpg',
+                  fruits_name: '干货'
+                },
+                {
+                  img_url: 'http://file06.16sucai.com/2016/0502/19b572a189ec13d9cdcac82f579d680a.jpg',
+                  fruits_name: '干货'
+                }
+              ],
+              [
+                {
+                  img_url: 'http://file06.16sucai.com/2016/0502/19b572a189ec13d9cdcac82f579d680a.jpg',
+                  fruits_name: '干货'
+                },
+                {
+                  img_url: 'http://file06.16sucai.com/2016/0502/19b572a189ec13d9cdcac82f579d680a.jpg',
+                  fruits_name: '干货'
+                },
+                {
+                  img_url: 'http://file06.16sucai.com/2016/0502/19b572a189ec13d9cdcac82f579d680a.jpg',
+                  fruits_name: '干货'
+                },
+                {
+                  img_url: 'http://pic13.nipic.com/20110301/2531170_082157389664_2.jpg',
+                  fruits_name: '干货'
+                }
+              ]
+            ]
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .product {
+
+  }
+
+  .fruit_title {
+
+  }
+
+  .truit_item {
+    margin-left: 120px;
+  }
+
+  .truit_item img, h3 {
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+
+  .tableClass {
+    margin: 80px auto;
+  }
+
+  .fruit_img {
+    width: 300px;
+    height: 300px;
+  }
+</style>
