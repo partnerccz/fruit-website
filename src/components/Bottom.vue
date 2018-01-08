42362c96ee3f48bf70a0c21ff49e1decf49dd448
partnerccz <2779017796@qq.com>
2018-01-08 14:32:30 +0800

    通用Bottom文件提交

---
+++ src/components/Bottom.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bottom" style="margin:0px;font-size:10px;background-color: #66a812;color:#f5faf0;height: 35px;width:100%;line-height: 35px;">
+      广州指猴互联网科技有限公司 CopyRight 2015-2018 okguo.com 版权所有 粤ICP备15027534号-1
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'bottom'
+  }
+</script>
+
+<style scoped>
+
+</style>
